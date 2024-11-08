3f2d1ca727d1bd6f1b9b89d1bf65a63726a047b2
Pedro Henrique <pedrohenriquedevprog@gmail.com>
2024-11-07 21:43:51 -0300

    TypeScript funcionando com express

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
 
+import express, {Request,Response} from 'express'
 
-console.log("ola mundo")
+const app = express()
+const PORT = process.env.PORT || 5001
+
+app.get('/',(req,res) =>{
+    res.send('Hellor World')
+})
+
+app.listen(PORT,() =>{
+    console.log('Server is Running ')
+})
+
