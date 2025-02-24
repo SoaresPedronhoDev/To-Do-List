bc51c7c9bd3d71e788fdef63b883578d8a108db1
Pedro Henrique <pedrohenriquedevprog@gmail.com>
2025-02-24 20:40:21 -0300

    arquivo model User alterado para receber registro com google

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,28 +1,31 @@
 
 import mongoose, { Schema, Document } from "mongoose";
 
-//Definicao de interface do usuario 
+//definicao de interface do usuario 
 
  interface IUser extends Document {
     email : string;
-    password : string;
+    password? : string;
+    googleId? : string;
+    displayName? : string;
     createdAt : Date;
     updatedAt : Date;
 }
 
-//Criando esquema do usuario    
+//criando esquema do usuario    
 
-const UserSchema : Schema = new Schema(
-    {
-    email : { type : String, required : true, unique : true },
-    password : { type : String, required : true },
-    },
-    { timestamps: true }
-)
+const userSchema: Schema = new Schema({
+    email: { type: String, required: true, unique: true },
+    password: { type: String },
+    googleId: { type: String, unique: true, sparse: true }, 
+    displayName: { type: String },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+  });
 
 //criando modelo do usuario
 
-const User = mongoose.model<IUser>('User', UserSchema);
+const User = mongoose.model<IUser>('User', userSchema);
 
 export default User; //exportando modelo do usuario
         
